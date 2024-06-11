a06ee3449fa732101e6b66b359d8c46f3c65aa18
Isabella <isabellaotoo25@gmail.com>
2024-06-11 19:58:29 +0000

    Add Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,18 @@
 
 import React from 'react';
+import Slides from '../inc/Slides';
 
 const Home = () => {
   return (
+    <div>
+      <Slides />
     <div className="container">
       <div className="card shadow">
         <div className="card-body">
         <button className="btn btn-primary">Home</button>
           </div>   
           </div>  
+    </div>
     </div>
   )
 }
