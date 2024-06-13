82876f459bd71292d77a4cab63188aaf394d4dc1
Isabella <isabellaotoo25@gmail.com>
2024-06-13 10:41:51 +0000

    Add Vmc.js

---
+++ src/components/pages/inc/Vmc.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Vmc = () => {
+  return (
+    <div>
+    <section className='section bg-c-light border-top'>
+    <div className='container'>
+        <div className='row'>
+          <div className='col-md-12 mb-5 text-center'>
+            <h3 className='main-heading'>Our Vision, Mission and Values</h3>
+            <div className='underline mx-auto'></div>
+          </div>
+          <div className='col-md-4 text-center'>
+            <h6>Our Vision</h6>
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old</p>
+          </div>
+          <div className='col-md-4 text-center'>
+            <h6>Our Mission</h6>
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old</p>
+          </div>
+          <div className='col-md-4 text-center'>
+            <h6>Our Core Values</h6>
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old</p>
+          </div>
+          
+        </div>
+      </div>
+
+    </section>
+      
+    </div>
+  )
+}
+
+export default Vmc
