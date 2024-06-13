94e93f3b1e7b0096a1ca8ab4d0c77a20efce27c6
Isabella <isabellaotoo25@gmail.com>
2024-06-13 10:31:23 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import './App.css';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Navbar from "./components/inc/Navbar";
 import Home from './components/pages/Home';
 import About from './components/pages/About';
@@ -7,14 +8,14 @@ import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
-    </BrowserRouter>
+    </Router>
     
   );
 }
