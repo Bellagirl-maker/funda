9297234e5a3cf84ad12ab1552108995541913495
Isabella <isabellaotoo25@gmail.com>
2024-06-14 18:35:03 +0000

    Update Navbar.js

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg bg-dark">
   <div className="container-fluid">
-    <Link to="/" className="navbar-brand">Funda of IT</Link>
+    <Link to="/" className="navbar-brand text-white">Funda of IT</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link to="/" className="nav-link active" aria-current="page">Home</Link>
+          <Link to="/" className="nav-link active text-white" aria-current="page">Home</Link>
         </li>
         <li className="nav-item">
-            <Link to="/about" className="nav-link">About Us</Link>
+            <Link to="/about" className="nav-link text-white">About Us</Link>
         </li>
         <li className="nav-item">
-        <Link to="/contact" className="nav-link">Contact Us</Link>
+        <Link to="/contact" className="nav-link text-white">Contact Us</Link>
         </li>
       </ul>
       
