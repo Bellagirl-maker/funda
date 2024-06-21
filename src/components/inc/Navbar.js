ca1c261aa8f26ad33d53c64f0c0afaf61650d60f
Isabella <isabellaotoo25@gmail.com>
2024-06-21 20:54:32 +0000

    Update Navbar.js

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark fixed-top">
+    <nav className="navbar navbar-expand-lg bg-dark p-2 fixed-top">
   <div className="container-fluid">
     <Link to="/" className="navbar-brand text-white">Funda of IT</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <Link to="/" className="nav-link active text-white" aria-current="page">Home</Link>
         </li>
